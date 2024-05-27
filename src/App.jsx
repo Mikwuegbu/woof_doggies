880210ea2ca8dfb060d2298542e3419c333d7e27
Mikwuegbu <mstephen130@yahoo.com>
2024-05-27 12:05:44 +0100

    shared layout

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,19 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Dogs from './pages/Dogs';
 import Contacts from './pages/Contacts';
+import SharedLayout from './pages/SharedLayout';
 
 function App() {
 	return (
 		<>
 			<BrowserRouter>
 				<Routes>
-					<Route path="/" element={<Home />} />
-					<Route path="/dogs" element={<Dogs />} />
-					<Route path="/contacts" element={<Contacts />} />
-					<Route path="*" element={<h1>404 Not Found</h1>} />
+					<Route path="/" element={<SharedLayout />}>
+						<Route index element={<Home />} />
+						<Route path="/dogs" element={<Dogs />} />
+						<Route path="/contacts" element={<Contacts />} />
+						<Route path="*" element={<h1>404 Not Found</h1>} />
+					</Route>
 				</Routes>
 			</BrowserRouter>
 		</>
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import { Outlet } from 'react-router-dom';
+
+const Navbar = () => {
+	return (
+		<div>
+			<div>Sample Navbar</div>
+		</div>
+	);
+};
+
+export default Navbar;
