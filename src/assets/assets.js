8823db86f76ec64f3b66b64a0c49816e115fd3ed
Mikwuegbu <mstephen130@yahoo.com>
2024-05-27 13:11:40 +0100

    designing sharedlayout

---
+++ src/assets/assets.js
@@ -0,0 +1,4 @@
+import vector1 from './vector1.png';
+import woofprint from './woofprint.png';
+
+export { vector1, woofprint };
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,25 @@
-import { Outlet } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import { vector1, woofprint } from '../assets/assets';
 
 const Navbar = () => {
 	return (
-		<div>
-			<div>Sample Navbar</div>
-		</div>
+		<>
+			<div className="flex space-x-52 relative overflow-hidden">
+				<div className="w-[569.81px] h-[412.9px]">
+					<img src={vector1} alt={vector1} />
+				</div>
+				<img
+					src={woofprint}
+					alt={woofprint}
+					className="absolute w-48 h-24 top-12 -left-20"
+				/>
+				<div className="space-x-10 font-semibold text-4xl text-center py-10 font-baloo text-[#BC6A4D]">
+					<Link className="">Home</Link>
+					<Link>dogs</Link>
+					<Link>Contact us</Link>
+				</div>
+			</div>
+		</>
 	);
 };
 
