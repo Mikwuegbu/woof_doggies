dfef0fdc8a30fab44a7c45bad7c910e6d026ce4c
Mikwuegbu <mstephen130@yahoo.com>
2024-05-27 11:34:22 +0100

    router-dom installed

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,21 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Dogs from './pages/Dogs';
+import Contacts from './pages/Contacts';
+
 function App() {
-	return <></>;
+	return (
+		<>
+			<BrowserRouter>
+				<Routes>
+					<Route path="/" element={<Home />} />
+					<Route path="/dogs" element={<Dogs />} />
+					<Route path="/contacts" element={<Contacts />} />
+					<Route path="*" element={<h1>404 Not Found</h1>} />
+				</Routes>
+			</BrowserRouter>
+		</>
+	);
 }
 
 export default App;
