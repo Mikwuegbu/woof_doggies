588847eeec7262bc0015180e92e858cf041ab27b
Mikwuegbu <mstephen130@yahoo.com>
2024-05-30 13:11:28 +0100

    completed the dog page desktop version

--- src/pages/Dogs.jsx
+++ src/pages/Dogs.jsx
@@ -1,45 +1,51 @@
 import { searchicon, woofBtn, dog1, dog2, dog3 } from '../assets/assets';
+import useFetch from '../components/useFetch';
 
-const Dogs = () => (
-	<div className=" flex relative justify-center">
-		<div className="absolute -top-40 flex justify-center items-center">
-			<div className="flex items-center bg-[#FCF8F6] px-3 py-3 pl-5 rounded-full">
-				<img src={searchicon} alt={searchicon} className="w-4 h-4 mb-1" />
-				<input
-					type="text"
-					className="placeholder:font-baloo placeholder:text-3xl placeholder:text-[#BC6A4D] px-3.5 py-5 w-72 outline-none bg-transparent"
-					placeholder="Search Dog bread"
-				/>
-				<button className="bg-[#BC6A4D] flex items-center rounded-full px-8">
-					<img src={woofBtn} alt={woofBtn} className="w-10 h-10 " />
-					<div className="text-white font-baloo text-3xl font-semibold py-6 px-3">
-						dogs
+const Dogs = () => {
+	const dogApi = 'https://api.thedogapi.com/v1/images/search?limit=10';
+	const { data, loading, error } = useFetch(dogApi);
+
+	return (
+		<div className=" flex relative justify-center">
+			<div className="absolute -top-40 flex justify-center items-center">
+				<div className="flex items-center bg-[#FCF8F6] px-3 py-3 pl-5 rounded-full">
+					<img src={searchicon} alt={searchicon} className="w-4 h-4 mb-1" />
+					<input
+						type="text"
+						className="placeholder:font-baloo placeholder:text-3xl placeholder:text-[#BC6A4D] px-3.5 py-5 w-72 outline-none bg-transparent"
+						placeholder="Search Dog bread"
+					/>
+					<button className="bg-[#BC6A4D] flex items-center rounded-full px-8">
+						<img src={woofBtn} alt={woofBtn} className="w-10 h-10 " />
+						<div className="text-white font-baloo text-3xl font-semibold py-6 px-3">
+							dogs
+						</div>
+					</button>
+				</div>
+			</div>
+			<div className="pb-20">
+				{loading ? (
+					<div className="font-baloo text-xl text-[#BC6A4D]">
+						Loading Dogs...{' '}
 					</div>
-				</button>
+				) : (
+					<div className="grid grid-cols-3 gap-10 -p">
+						{data.map((dog) => (
+							<div
+								className="border-dashed border-2 rounded-3xl p-2 border-[#E4C3B8]"
+								key={dog.id}
+							>
+								<img
+									src={dog.url}
+									alt={dog.url}
+									className="w-48 h-56 rounded-xl"
+								/>
+							</div>
+						))}
+					</div>
+				)}
 			</div>
 		</div>
-		<div className="pb-20">
-			<div className="grid grid-cols-3 gap-10 -p">
-				<div className="border-dashed border-2 rounded-3xl p-2 border-[#E4C3B8]">
-					<img src={dog1} alt={dog1} className="w-48 h-56" />
-				</div>
-				<div className="border-dashed border-2 rounded-3xl p-2 border-[#E4C3B8]">
-					<img src={dog2} alt={dog2} className="w-48 h-56" />
-				</div>
-				<div className="border-dashed border-2 rounded-3xl p-2 border-[#E4C3B8]">
-					<img src={dog3} alt={dog3} className="w-48 h-56" />
-				</div>
-				<div className="border-dashed border-2 rounded-3xl p-2 border-[#E4C3B8]">
-					<img src={dog1} alt={dog1} className="w-48 h-56" />
-				</div>
-				<div className="border-dashed border-2 rounded-3xl p-2 border-[#E4C3B8]">
-					<img src={dog2} alt={dog2} className="w-48 h-56" />
-				</div>
-				<div className="border-dashed border-2 rounded-3xl p-2 border-[#E4C3B8]">
-					<img src={dog3} alt={dog3} className="w-48 h-56" />
-				</div>
-			</div>
-		</div>
-	</div>
-);
+	);
+};
 export default Dogs;
